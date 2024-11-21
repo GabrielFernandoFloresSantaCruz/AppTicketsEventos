d2c76338f8887004ae3219d1d607269e17b25d90
GabrielFloresSC <160886373+GabrielFloresSC@users.noreply.github.com>
2024-11-20 22:41:44 -0400

    Arreglar weas

--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -4,6 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CarritoService {
+  private cart: any[] = [];
 
   constructor() { }
+
+  addToCart(item: any): void {
+    this.cart.push(item);
+  }
+
+  getCartItems(): any[] {
+    return this.cart;
+  }
+
+  clearCart(): void {
+    this.cart = [];
+  }
 }
